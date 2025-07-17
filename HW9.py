a17813d1d04d981e837cbbaabea02c272fdc4c69
Aviralrathi314 <rathiaviral@gmail.com>
2025-07-17 11:18:13 +0530

    Add files via upload

---
+++ HW9.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age"))
+if age>20:
+    if age>20 and age<=30:
+        print("You are allowed to come in the playclub")
+    else:
+        print("You are not allowed in the playclub")
+else:
+    print("You are too young")
